38ecf146fbb037e9954ed0a6e29f573348930343
Matthieu Jacques <74052376+Matthieu-Jck@users.noreply.github.com>
2023-11-07 01:14:24 +0100

    Update api_getter.rb

--- .github/scripts/api_getter.rb
+++ .github/scripts/api_getter.rb
@@ -1,33 +1,32 @@
 require 'net/http'
 require 'json'
 
-url = URI("https://api.nasa.gov/insight_weather/?api_key=qlDL7aSsPYvtXmlOhswtisUdfyAfrP4xrpwQQOFn&feedtype=json&ver=1.0")
+# Replace with your actual city ID and API key from OpenWeatherMap
+city_id = '2972315'
+api_key = '8763fab78dfe2059f03dbe174ced5d97'
+url = URI("http://api.openweathermap.org/data/2.5/weather?id=#{city_id}&appid=#{api_key}&units=metric")
+
 response = Net::HTTP.get(url)
 data = JSON.parse(response)
 
-# Get the first available sol
-first_sol = data['sol_keys'].first
+if data && data["weather"]
+  weather_data = data
 
-# Get the weather data for the first available sol
-weather_data = data[first_sol]
-
-# Create or open README.md and write the data to it
-File.open("README.md", "w") do |file|
-  file.puts "# Mars Weather Report\n"
-  file.puts "Weather data for Sol #{first_sol}:\n"
-  file.puts "- Season: #{weather_data['Season']}"
-  file.puts "- First UTC: #{weather_data['First_UTC']}"
-  file.puts "- Last UTC: #{weather_data['Last_UTC']}"
-  file.puts "\n**Atmospheric Temperature (°C):**"
-  file.puts "- Average: #{weather_data['AT']['av']}"
-  file.puts "- Minimum: #{weather_data['AT']['mn']}"
-  file.puts "- Maximum: #{weather_data['AT']['mx']}"
-  file.puts "\n**Horizontal Wind Speed (m/s):**"
-  file.puts "- Average: #{weather_data['HWS']['av']}"
-  file.puts "- Minimum: #{weather_data['HWS']['mn']}"
-  file.puts "- Maximum: #{weather_data['HWS']['mx']}"
-  file.puts "\n**Atmospheric Pressure (Pa):**"
-  file.puts "- Average: #{weather_data['PRE']['av']}"
-  file.puts "- Minimum: #{weather_data['PRE']['mn']}"
-  file.puts "- Maximum: #{weather_data['PRE']['mx']}"
+  File.open("README.md", "w") do |file|
+    file.puts "# Current Weather Report\n"
+    file.puts "Current weather data for the selected location:\n"
+    file.puts "- Weather Condition: #{weather_data['weather'].first['main']}"
+    file.puts "- Description: #{weather_data['weather'].first['description']}"
+    file.puts "- Temperature: #{weather_data['main']['temp']}°C"
+    file.puts "- Feels Like: #{weather_data['main']['feels_like']}°C"
+    file.puts "- Minimum Temperature: #{weather_data['main']['temp_min']}°C"
+    file.puts "- Maximum Temperature: #{weather_data['main']['temp_max']}°C"
+    file.puts "- Pressure: #{weather_data['main']['pressure']} hPa"
+    file.puts "- Humidity: #{weather_data['main']['humidity']}%"
+    file.puts "- Wind Speed: #{weather_data['wind']['speed']} m/s"
+    file.puts "- Wind Degree: #{weather_data['wind']['deg']}°"
+  end
+else
+  puts "Weather data not available for the provided location"
 end
+
